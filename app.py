51e3764bf7ec6c1cca1f0fb4e1ca89db8632f7c3
sujintee <111175315+sujintee@users.noreply.github.com>
2022-08-13 16:38:37 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask,render_template,request
+
+
+# In[2]:
+
+
+import joblib
+
+
+# In[3]:
+
+
+app = Flask(__name__)
+
+
+# In[4]:
+
+
+@app.route("/",methods = ["GET","POST"])
+def index():
+    if request.method =="POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
+        model1 = joblib.load("regression_DBS")
+        pred1 = model1.predict([[rates]])
+        model2 = joblib.load("tree_DBS")
+        pred2 = model2.predict([[rates]])
+        return(render_template("index.html",result1=pred1,result2=pred2))
+    else:
+        return(render_template("index.html",result1="test",result2="tes"))
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5002)
+
+
+# In[ ]:
+
+
+__name__
+
